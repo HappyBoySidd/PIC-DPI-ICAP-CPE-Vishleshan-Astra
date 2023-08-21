3fe9425ecb3fd4113391bffc506b05a979d7c56e
Siddharth Mukherjee <Siddharth.Mukherjee@philips.com>
2023-08-21 12:38:09 +0530

    changes to use Spectre.Console library

---
+++ Vishleshan-Astra/Astra.Infrastructure/ActivityService.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static Vishleshan_Astra.Astra_Core.Action;
+using Microsoft.Win32;
+using System.Configuration;
+using System.Collections.Specialized;
+using System.IO;
+using System.Globalization;
+
+namespace Vishleshan_Astra.Astra.Infrastructure
+{
+    class ActivityService
+    {
+        List<KeyValuePair<string, DateTime>> FileMetadata { get; set; }
+
+        public List<Tuple<string, string, DateTime, DateTime>> CheckFileSanity()
+        {
+            if (!CheckConfiguration())
+                return new List<Tuple<string, string, DateTime, DateTime>>();
+
+            var lstFilesWithDiscrepancy = new List<Tuple<string, string, DateTime, DateTime>>();
+            foreach (var objFileMetadata in FileMetadata)
+            {
+                var dtCheckIn = objFileMetadata.Value;
+                var dtLastModified = File.GetLastWriteTime(objFileMetadata.Key);
+                if (dtLastModified < dtCheckIn)
+                    lstFilesWithDiscrepancy.Add(Tuple.Create(Path.GetFileName(objFileMetadata.Key), objFileMetadata.Key, dtCheckIn, dtLastModified));
+            }
+
+            return lstFilesWithDiscrepancy;
+        }
+
+        public string PublishListOfUnsafeFiles(List<Tuple<string, string, DateTime, DateTime>> lstUnsafeFiles)
+        {
+            var strLogDirectory = GetDirectoryToLog();
+            strLogDirectory = Path.Combine(path1: strLogDirectory, path2: "UnsafeFiles");
+
+            if (!Directory.Exists(strLogDirectory))
+                Directory.CreateDirectory(strLogDirectory);
+
+            return SaveToCSVFile(lstUnsafeFiles, strLogDirectory);
+        }
+
+        public bool CheckConfiguration()
+        {
+            GetFilesToBeVerified();
+            if (!FileMetadata.Any())
+                return false;
+            return true;
+        }
+
+        private static string GetDirectoryToLog()
+        {
+            var strCurrentBinDirectory = Environment.CurrentDirectory;
+            return Directory.GetParent(path: strCurrentBinDirectory).Parent?.FullName;
+        }
+
+        private static string SaveToCSVFile(List<Tuple<string, string, DateTime, DateTime>> lstFilesWithDiscrepancy, string strDirectory)
+        {
+            if (!lstFilesWithDiscrepancy.Any())
+                return string.Empty;
+
+            var lstLines = new List<string> { "File Name,File Path,Check In Date,Last Modified Date" };
+            foreach (var (strFileName, strFilePath, dtCheckIn, dtLastModified) in lstFilesWithDiscrepancy)
+            {
+                lstLines.Add(item: $"{strFileName},{strFilePath},{dtCheckIn},{dtLastModified}");
+            }
+
+            var strFileFullname = Path.Combine(strDirectory, $"UnsafeFiles-{DateTime.Now:dd'-'MMM'-'yyyy'T'HH'-'mm'-'ss}.csv");
+            File.WriteAllLines(strFileFullname, lstLines.ToArray());
+            return strFileFullname;
+        }
+
+        private void GetFilesToBeVerified()
+        {
+            var lstFilesToBeChecked = new List<string>();
+            var lstValidConfigurations = GetConfigurationsForCurrentEnvironment();
+            foreach (var strFilePath in lstValidConfigurations)
+            {
+                var strValidatedFile = CheckIfFileExist(strFilePath.Key);
+                lstFilesToBeChecked.Add(strValidatedFile);
+            }
+
+            FileMetadata = lstValidConfigurations.Where(kv => lstFilesToBeChecked.Contains(kv.Key)).ToList();
+        }
+
+        private List<KeyValuePair<string, DateTime>> GetConfigurationsForCurrentEnvironment()
+        {
+            RegistryKey keyMachine = Registry.Users.OpenSubKey(@".DEFAULT\Software\Philips Healthcare\CT", false);
+            if (keyMachine == null)
+                return new List<KeyValuePair<string, DateTime>>();
+
+            var machineType = (MachineType)Convert.ToInt32(keyMachine.GetValue("SystemType"));
+            var lstFiles = new List<KeyValuePair<string, DateTime>>();
+            switch (machineType)
+            {
+                case MachineType.Client:
+                    var clientSection = ConfigurationManager.GetSection("clientConfiguration") as NameValueCollection;
+                    foreach (var key in clientSection.AllKeys)
+                    {
+                        lstFiles.Add(new KeyValuePair<string, DateTime>(key, DateTime.ParseExact(clientSection.GetValues(key).FirstOrDefault(), "dd-MMM-yy", CultureInfo.InvariantCulture)));
+                    }
+                    break;
+                case MachineType.Server:
+                    var serverSection = ConfigurationManager.GetSection("serverConfiguration") as NameValueCollection;
+                    foreach (var key in serverSection.AllKeys)
+                    {
+                        lstFiles.Add(new KeyValuePair<string, DateTime>(key, DateTime.ParseExact(serverSection.GetValues(key).FirstOrDefault(), "dd-MMM-yy", CultureInfo.InvariantCulture)));
+                    }
+                    break;
+            }
+
+            return lstFiles;
+        }
+
+        private string CheckIfFileExist(object objFilePath)
+        {
+            var strFilePath = Convert.ToString(objFilePath);
+            if (string.IsNullOrWhiteSpace(strFilePath)) throw new ArgumentException(message: "File Path cannot be blank ");
+
+            var strFileName = Path.GetFileName(strFilePath);
+            if (string.IsNullOrWhiteSpace(strFileName)) throw new ArgumentException(message: $"File Name cannot be blank >> Path: {strFilePath}");
+
+            return strFilePath;
+        }
+
+        private enum MachineType
+        {
+            Server = 3,
+            Client = 4
+        }
+    }
+}
---
+++ Vishleshan-Astra/Astra.UI/ConsoleUI.cs
@@ -0,0 +1,137 @@
+﻿using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static Vishleshan_Astra.Astra_Core.Action;
+
+namespace Vishleshan_Astra.Astra.UI
+{
+    static class ConsoleUI
+    {
+        public static Actions GetActivityToPerform(string[] arguments)
+        {
+            if (arguments.Length > 0 && !string.IsNullOrWhiteSpace(arguments[0]) && int.TryParse(arguments[0], out var result))
+                return (Actions)result;
+
+            return (Actions)GetUserInput();
+        }
+
+        /// <summary>
+        /// Parses the user input / console arguments
+        /// </summary>
+        /// <returns>Returns the console argument passed (if any) or the user input</returns>
+        private static int GetUserInput()
+        {
+            var strMessage = new StringBuilder();
+            strMessage.AppendLine("[blue]Select the task to be performed from the list of actions given below");
+            strMessage.AppendLine("(1) => Check configuration");
+            strMessage.AppendLine("(2) => Verify File Sanity");
+            strMessage.AppendLine("(3) => Notify relevant stakeholders");
+            strMessage.AppendLine("(4) => Exit");
+            strMessage.AppendLine("Provide your input: [/]");
+
+            return AnsiConsole.Prompt(
+            new TextPrompt<int>(strMessage.ToString())
+                .PromptStyle("green")
+                .ValidationErrorMessage("[red]Please enter valid option[/]")
+                .Validate(option =>
+                {
+                    if (option < 1 || option > 4)
+                        return ValidationResult.Error("[red]Invalid option entered[/]");
+                    return ValidationResult.Success();
+                }));      
+        }
+
+        public static async Task PrintListOfUnsafeFilesAsync(List<Tuple<string, string, DateTime, DateTime>> lstUnsafeFiles)
+        {
+            var tblUnsafeFiles = new Table().Expand().BorderColor(Color.Grey); 
+
+            var numRowCounter = 0;
+
+            await AnsiConsole.Live(tblUnsafeFiles)
+            .AutoClear(false)   // Do not remove when done
+            .Overflow(VerticalOverflow.Ellipsis) // Show ellipsis when overflowing
+            .Cropping(VerticalOverflowCropping.Top) // Crop overflow at top
+            .StartAsync(async ctx =>
+            {
+                tblUnsafeFiles.AddColumn(new TableColumn("[gold3_1]#[/]").Centered());
+                ctx.Refresh();
+                Thread.Sleep(200);
+                tblUnsafeFiles.AddColumn(new TableColumn("[gold3_1]File Name[/]").Centered());
+                ctx.Refresh();
+                Thread.Sleep(200);
+                tblUnsafeFiles.AddColumn(new TableColumn("[gold3_1]File Path[/]").Centered());
+                ctx.Refresh();
+                Thread.Sleep(200);
+                tblUnsafeFiles.AddColumn(new TableColumn("[gold3_1]Check In Date[/]").Centered());
+                ctx.Refresh();
+                Thread.Sleep(200);
+                tblUnsafeFiles.AddColumn(new TableColumn("[gold3_1]Last Modified Date[/]").Centered());
+                ctx.Refresh();
+                Thread.Sleep(200);
+                do
+                {
+                    AddUnsafeFile(tblUnsafeFiles, numRowCounter, lstUnsafeFiles[numRowCounter].Item1, lstUnsafeFiles[numRowCounter].Item2, lstUnsafeFiles[numRowCounter].Item3, lstUnsafeFiles[numRowCounter].Item4);
+                    numRowCounter++;
+                    ctx.Refresh();
+                    Thread.Sleep(200);
+                }
+                while (numRowCounter <= lstUnsafeFiles.Count);
+                await Task.Delay(200);
+            });
+
+            #region old code
+            //tblUnsafeFiles.AddColumn(new TableColumn("[gold3_1]#[/]").Centered());
+            //tblUnsafeFiles.AddColumn(new TableColumn("[gold3_1]File Name[/]").Centered());
+            //tblUnsafeFiles.AddColumn(new TableColumn("[gold3_1]File Path[/]").Centered());
+            //tblUnsafeFiles.AddColumn(new TableColumn("[gold3_1]Check In Date[/]").Centered());
+            //tblUnsafeFiles.AddColumn(new TableColumn("[gold3_1]Last Modified Date[/]").Centered());
+
+            //foreach (var (strFileName, strFilePath, dtCheckIn, dtLastModified) in lstUnsafeFiles)
+            //{
+            //    tblUnsafeFiles.AddRow($"[orangered1]{numRowCounter++}[/]", $"[orangered1]{strFileName}[/]", $"[orangered1]{strFilePath}[/]", $"[orangered1]{dtCheckIn}[/]", $"[orangered1]{dtLastModified}[/]");
+            //}
+            //tblUnsafeFiles.Expand();
+            #endregion
+            AnsiConsole.Write(tblUnsafeFiles);
+        }
+        private static void AddUnsafeFile(Table tblUnsafeFiles, int numRowCounter, string strFileName, string strFilePath, DateTime dtCheckIn, DateTime dtLastModified)
+        {
+            tblUnsafeFiles.AddRow($"[orangered1]{numRowCounter}[/]", $"[orangered1]{strFileName}[/]", $"[orangered1]{strFilePath}[/]", $"[orangered1]{dtCheckIn}[/]", $"[orangered1]{dtLastModified}[/]");
+        }
+
+        #region Logging methods
+        public static void PrintMessage(string strMessage, LogType logType = LogType.Information)
+        {
+            string strColour;
+            switch (logType)
+            {
+                case LogType.Warning:
+                    strColour = "orange3";
+                    break;
+                case LogType.Error:
+                    strColour = "red";
+                    break;
+                case LogType.Special:
+                    strColour = "gold3_1";
+                    break;
+                default:
+                    strColour = "blue";
+                    break;
+            }
+
+            AnsiConsole.MarkupInterpolated($"[{strColour}]{strMessage}[/]");
+        }
+
+        public enum LogType
+        {
+            Information,
+            Warning,
+            Error,
+            Special
+        }
+        #endregion
+    }
+}
